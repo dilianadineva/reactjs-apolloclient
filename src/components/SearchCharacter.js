7c0697d38c49697a875bf806842b465309f6bc23
dilianadineva <mnyaha@gmail.com>
2023-10-23 14:29:03 +0300

    Update SearchCharacter.js

--- src/components/SearchCharacter.js
+++ src/components/SearchCharacter.js
@@ -40,7 +40,7 @@ function SearchCharacter() {
           <SearchIcon />
         </SearchIconWrapper>
         <StyledInputBase
-          placeholder='Search for a location'
+          placeholder='Search for a character'
           inputProps={{ 'aria-label': 'search character' }}
           onChange={(e) => {
             setName(e.target.value);
